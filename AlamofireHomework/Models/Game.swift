c0e5616c7627d8411461ab30e3bd44cbb8e9fd21
Vsevolod <vsevolod.lashin@gmail.com>
2023-05-14 20:42:56 +0300

    Alamofire added

---
+++ AlamofireHomework/Models/Game.swift
@@ -0,0 +1,26 @@
+//
+//  Game.swift
+//  AlamofireHomework
+//
+//  Created by Vsevolod Lashin on 14.05.2023.
+//
+
+import Foundation
+
+struct Game: Decodable {
+    let id: Int
+    let title: String
+    let thumbnail: URL
+    let description: String?
+    let gameUrl: URL
+    let genre: String
+    let platform: String
+    let publisher: String
+    let developer: String
+    let releaseDate: String
+    let screenshots: [Screenshot]?
+}
+
+struct Screenshot: Decodable, Equatable {
+    let image: URL
+}
---
+++ AlamofireHomework/Services/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  AlamofireHomework
+//
+//  Created by Vsevolod Lashin on 14.05.2023.
+//
+
+import Foundation
