8574642fa68f5a527ea907ebb974ca6ac860a043
Vsevolod <vsevolod.lashin@gmail.com>
2023-05-14 22:13:56 +0300

    fetchGames function added

--- AlamofireHomework/Services/NetworkManager.swift
+++ AlamofireHomework/Services/NetworkManager.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import Alamofire
+
+final class NetworkManager {
+    static var shared = NetworkManager()
+    
+    private init() {}
+    
+    func fetchGames(with url: URL, completion: @escaping(Result<[Game], AFError>) -> Void) {
+        AF.request(url)
+            .validate()
+            .responseJSON { dataResponse in
+                switch dataResponse.result {
+                case .success(let value):
+                    let games = Game.getGames(from: value)
+                    completion(.success(games))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
+    }
+}
--- AlamofireHomework/ViewControllers/ViewController.swift
+++ AlamofireHomework/ViewControllers/ViewController.swift
@@ -9,13 +9,26 @@ import UIKit
 
 final class ViewController: UIViewController {
 
+    private let networkManager = NetworkManager.shared
+    private var allGames: [Game] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         fetchGames()
     }
     
     private func fetchGames() {
+        let gamesURL = URL(string: "https://www.freetogame.com/api/games")!
         
+        networkManager.fetchGames(with: gamesURL) { [weak self] result in
+            switch result {
+            case .success(let games):
+                self?.allGames = games
+                print(games)
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 }
 
